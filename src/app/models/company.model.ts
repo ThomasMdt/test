d870109b506f28e28f393a104bffd5087c049cf2
Makhyanov <makhyanov.s@orionnet.ru>
2021-10-21 14:19:15 +0700

    внес правки

--- src/app/models/company.model.ts
+++ src/app/models/company.model.ts
@@ -19,31 +19,31 @@ export class CompanyBuilder {
         name: 'Эконом',
         cost: 4,
         limitWeight: 20,
-        freeWeight: 5,
         overpayCost: 4000,
         overpayWeight: 5,
         discountAge: 0,
-        discountPercent: 0
+        discountPercent: 0,
+        overpayByKg: false,
       }),
       new Tariff({
         name: 'Продвинутый',
         cost: 8,
         limitWeight: 50,
-        freeWeight: 20,
         overpayCost: 5000,
         overpayWeight: 20,
         discountAge: 7,
-        discountPercent: 30
+        discountPercent: 30,
+        overpayByKg: false
       }),
       new Tariff({
         name: 'Люкс',
         cost: 15,
         limitWeight: 50,
-        freeWeight: Number.MAX_VALUE,
         overpayCost: 5000,
         overpayWeight: Number.MAX_VALUE,
         discountAge: 16,
-        discountPercent: 30
+        discountPercent: 30,
+        overpayByKg: false
       })
     ];
     return model;
@@ -55,31 +55,31 @@ export class CompanyBuilder {
         name: 'Эконом',
         cost: 0.5,
         limitWeight: 50,
-        freeWeight: 15,
         overpayCost: 500,
         overpayWeight: 5,
         discountAge: 5,
-        discountPercent: 50
+        discountPercent: 50,
+        overpayByKg: true
       }),
       new Tariff({
         name: 'Продвинутый',
         cost: 2,
         limitWeight: 50,
-        freeWeight: 20,
         overpayCost: 5000,
         overpayWeight: 20,
         discountAge: 8,
-        discountPercent: 30
+        discountPercent: 30,
+        overpayByKg: true
       }),
       new Tariff({
         name: 'Люкс',
         cost: 4,
         limitWeight: 60,
-        freeWeight: Number.MAX_VALUE,
         overpayCost: 5000,
         overpayWeight: Number.MAX_VALUE,
         discountAge: 16,
-        discountPercent: 20
+        discountPercent: 20,
+        overpayByKg: false
       })
     ];
     return model;
